6ea5070b3fe0dfcd0085fe56d876b6fb8844a203
zxl <925399505@qq.com>
2020-03-25 21:38:04 +0800

    one

---
+++ src/test/java/com/baizhi/TestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("哈哈哈");
+    }
+
+}
